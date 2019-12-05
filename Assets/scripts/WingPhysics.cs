b10cf3b1911ce93aea302f332915ee86ed426033
Thomas Haller <Thomas.Haller@lab10.coop>
2019-12-05 19:27:52 +0100

    some playaround with physics. working on displaying the forces

--- Assets/scripts/WingPhysics.cs
+++ Assets/scripts/WingPhysics.cs
@@ -7,9 +7,33 @@ public class WingPhysics : MonoBehaviour
     public float wingSpan = 13.56f;
     public float wingArea = 78.04f;
 
+    public Material forceDebugMaterial;
+
+    public bool showForceDebug = true;
+
+
+
     private float aspectRatio;
     private Rigidbody rigidBody;
 
+    private Vector3 lastWingForce;
+
+    //private GameObject forceDebugVisual;
+
+    private static readonly int[] triangles = {
+            0, 2, 1, //face front
+            0, 3, 2,
+            2, 3, 4, //face top
+            2, 4, 5,
+            1, 2, 5, //face right
+            1, 5, 6,
+            0, 7, 4, //face left
+            0, 4, 3,
+            5, 4, 7, //face back
+            5, 7, 6,
+            0, 6, 7, //face bottom
+            0, 1, 6
+        };
     private void calculateForces()
     {
         // *flip sign(s) if necessary*
@@ -34,25 +58,112 @@ public class WingPhysics : MonoBehaviour
 
         var vLift = liftDirection * lift;
         var vDrag = dragDirection * drag;
-        var vTotalForce = vLift - vDrag;
+        lastWingForce = vLift - vDrag;
 
         // Lift + Drag = Total Force
-        Debug.Log(vLift + " + " + vDrag + " = " + vTotalForce);
-        rigidBody.AddForce(vTotalForce);
+        //Debug.Log(vLift + " + " + vDrag + " = " + vTotalForce);
+
+        //Debug.DrawLine(rigidBody.transform.position ,rigidBody.transform.position + lastWingForce * 100000000, Color.green, 1);
+        
+        
+        rigidBody.AddForce(lastWingForce);
+
+        
+
 
         //theBird.AddForce(transform.forward * EnginePower);
     }
+
+    void OnPostRender()
+    {
+        // Debug.Log("Entering OnPostRender");
+        // if (showForceDebug)
+        // {
+        //     if (!forceDebugMaterial)
+        //     {
+        //         Debug.LogError("Please Assign a material on the inspector");
+        //         return;
+        //     }
+        //     GL.PushMatrix();
+        //     forceDebugMaterial.SetPass(0);
+        //     GL.LoadOrtho();
+        //     GL.Begin(GL.LINES);
+        //     GL.Color(Color.red);
+        //     GL.Vertex(rigidBody.position);
+        //     GL.Vertex(rigidBody.position + lastWingForce * 1000);
+        //     GL.End();
+        //     GL.PopMatrix();
+        // }
+        
+        
+        float xOffset = 10000000;
+        float yOffset = 10000000;
+        float zOffset = 10000000;
+
+        // Vector3[] vertices = {
+        //     new Vector3(rigidBody.position.x, rigidBody.position.y, rigidBody.position.z),
+        //     new Vector3(rigidBody.position.x + xOffset, rigidBody.position.y, rigidBody.position.z),
+        //     new Vector3(rigidBody.position.x + xOffset, rigidBody.position.y + yOffset, rigidBody.position.z),
+        //     new Vector3(rigidBody.position.x, rigidBody.position.y + yOffset, rigidBody.position.z),
+        //     new Vector3 (rigidBody.position.x, rigidBody.position.y + yOffset, rigidBody.position.z + zOffset),
+        //     new Vector3 (rigidBody.position.x + xOffset, rigidBody.position.y + yOffset, rigidBody.position.z + zOffset),
+        //     new Vector3 (rigidBody.position.x + xOffset, rigidBody.position.y, rigidBody.position.z + zOffset),
+        //     new Vector3 (rigidBody.position.x, rigidBody.position.y, rigidBody.position.z + zOffset)
+        // };
+
+        Vector3[] vertices = {
+            new Vector3(rigidBody.position.x, rigidBody.position.y, rigidBody.position.z),
+            new Vector3(rigidBody.position.x + xOffset, rigidBody.position.y, rigidBody.position.z),
+            new Vector3(rigidBody.position.x + xOffset, rigidBody.position.y + yOffset, rigidBody.position.z),
+            new Vector3(rigidBody.position.x, rigidBody.position.y + yOffset, rigidBody.position.z),
+            new Vector3 (rigidBody.position.x, rigidBody.position.y + yOffset, rigidBody.position.z + zOffset),
+            new Vector3 (rigidBody.position.x + xOffset, rigidBody.position.y + yOffset, rigidBody.position.z + zOffset),
+            new Vector3 (rigidBody.position.x + xOffset, rigidBody.position.y, rigidBody.position.z + zOffset),
+            new Vector3 (rigidBody.position.x, rigidBody.position.y, rigidBody.position.z + zOffset)
+        };
+
+
+        //Mesh mesh = Mesh.
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        Graphics.DrawMesh( mesh, new Vector3(), Quaternion.identity, forceDebugMaterial, 31, null, 0, null, false, false, false); 
+
+    }
+
+    
+
+    // void OnDrawGizmos()
+    // {
+    //     if (rigidBody)
+    //     {
+    //         Gizmos.DrawLine(rigidBody.centerOfMass, lastWingForce * 100000) ;
+    //     }
+    // }
 
     // Start is called before the first frame update
     void Start()
     {
         aspectRatio = (wingSpan * wingSpan) / wingArea;
         rigidBody = this.GetComponent<Rigidbody>();
+
+        // if (showForceDebug) {
+        //     //forceDebugVisual = GameObject.CreatePrimitive(PrimitiveType.) CreatePrimitive(PrimitiveType.Capsule);
+        //     GameObject go = new GameObject("WingForceDebug");
+            
+
+        //     Destroy(forceDebugVisual.GetComponent<Collider>());
+        //     forceDebugVisual.transform.parent = rigidBody.transform;
+        //     //forceDebugVisual.transform.position = new Vector3(2, 1, 0);
+        // }
     }
 
     // Update is called once per frame
     void Update()
     {  
         calculateForces();
+        OnPostRender();
     }
 }
